dc27a638c81676dd33336ed359ee9d599e263231
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-12-10 23:24:25 -0500

    fixed task a and finished task b

--- recursion.cpp
+++ recursion.cpp
@@ -12,19 +12,29 @@ This program works with recursion.
 #include "recursion.h"
 
 /**
-   Task A: This function prints all numbers in range left <= x <= right, separated by spaces.
+   Task A: This function prints all numbers in range left <= x <= right, separated by spaces. (Don’t use loops, global or static variables.)
 */
-std::string result;
 std::string printRange(int left, int right) {
   if(left > right) {
-    return result;
+    return "";
   }
   else {
     if(left <= right) {
-      result = result + std::to_string(left) + " ";
-      printRange(left + 1, right);
+      return std::to_string(left) + " " + printRange(left + 1, right);
     }
   }
 
-  return result;
+  return "";
+}
+
+/**
+   Task B: This function computes the sum of all numbers in range left <= x <= right. (Don’t use loops, global or static variables.)
+*/
+int sumRange(int left, int right) {
+  if(left > right) {
+    return 0;
+  }
+  else {
+    return left + sumRange(left + 1, right);
+  }
 }
