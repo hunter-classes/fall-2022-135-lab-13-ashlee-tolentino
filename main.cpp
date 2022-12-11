4fcf96bd8861955246adfff5c98d34643b3bb89d
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-12-10 23:04:51 -0500

    added task a example

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+Author: Ashlee Tolentino
+Course: CSCI-135
+Instructor: Michael Zamansky
+Assignment: Lab13
+
+This program works with recursion.
+*/
+
+#include <iostream>
+#include <string>
+#include "recursion.h"
+
+int main()
+{
+  // Task A
+  std::cout << "Task A\n\n";
+  std::string result1 = printRange(-2, 10);
+  std::cout << result1 << std::endl;
+  std::cout << "--------------------------------\n";
+
+
+  
+  return 0;
+}
