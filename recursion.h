86580d80a52bad8eff4f023e9e985e43067c221b
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-12-15 06:55:21 -0500

    added task d and e prototypes

--- recursion.h
+++ recursion.h
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 #pragma once
 
 std::string printRange(int left, int right);
 int sumRange(int left, int right);
 int sumArray(int *arr, int size);
-
+bool isAlphanumeric(std::string s);
+bool nestedParens(std::string s);
